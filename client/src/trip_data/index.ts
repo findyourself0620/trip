b93eea06594ef8770805412c36b477e252db0c47
findyourself0620 <findyourself0620@gmail.com>
2026-01-07 19:56:41 +0800

    Update index.ts
    Unregister sample trips (without deleting files

--- client/src/trip_data/index.ts
+++ client/src/trip_data/index.ts
@@ -1,12 +1,8 @@
 import { TripData } from '@/lib/types';
 import { guanziling } from './trips/guanziling';
-import { tainanCity } from './trips/tainan-city';
-import { coastal } from './trips/coastal';
 
 export const TRIPS_DATA: Record<string, TripData> = {
   [guanziling.id]: guanziling,
-  [tainanCity.id]: tainanCity,
-  [coastal.id]: coastal,
 };
 
 export * from '@/lib/types';
